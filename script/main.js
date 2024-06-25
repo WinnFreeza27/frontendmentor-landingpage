52d7244965c4e7d988c43d96878c1e5d6f4a4621
WinnFreeza27 <erwinpratamaeyr7@gmail.com>
2024-06-25 09:19:13 +0800

    Done

---
+++ script/main.js
@@ -0,0 +1,8 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/header-nav.css';
+import '../styles/section-home.css';
+import '../styles/section-feature.css';
+import '../styles/section-works.css';
+import '../styles/section-review.css';
+import '../styles/footer.css';
